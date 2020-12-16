85c26c53e397af41f6e76caabd9208fd8aa17ea4
shashikumar-gundala <shashikumar.gundala@lntinfotech.com>
2020-12-16 16:47:37 +0530

    added prime numbers code

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -8,8 +8,40 @@ namespace PrimeNumbers
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int i, j, flag;
+            Console.WriteLine("\nPrime numbers between " +
+                      "{0} and {1} are: ", a, b);
+            for (i = a; i <= b; i++)
+            {
+
+                // Skip 0 and 1 as they are
+                // niether prime nor composite
+                if (i == 1 || i == 0)
+                    continue;
+
+                // flag variable to tell
+                // if i is prime or not
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                // flag = 1 means i is prime
+                // and flag = 0 means i is not prime
+                if (flag == 1)
+                    Console.WriteLine(i);
+            }
         }
     }
 }
